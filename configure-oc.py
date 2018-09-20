149031d416d8bb56397608429c13a78d8693afb6
Cayo Hollanda <cayohollanda@hotmail.com>
2018-09-19 23:35:26 -0300

    add configure

---
+++ configure-oc.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#####################################
+# SCRIPT PARA CONFIGURAR O AMBIENTE #
+#####################################
+
+# Rodar comandos no bash
+import os
+
--- up-cluster.py
+++ up-cluster.py
@@ -13,4 +13,7 @@ public_hostname = raw_input("[?] DNS ou IP interno da máquina virtual -> ")
 routing_suffix = raw_input("[?] IP público da máquina virtual -> ")
 
 # Rodando script
-os.system("oc cluster up --skip-registry-check=true --public-hostname=%s --routing-suffix=%s" %(public_hostname, routing_suffix));
+commandRun = os.system("oc cluster up --skip-registry-check=true --public-hostname=%s --routing-suffix=%s" %(public_hostname, routing_suffix))
+
+if commandRun == 32512:
+    print("Erro ao executar comando. Verifique se o OpenShift Client Tools está instalado.")
