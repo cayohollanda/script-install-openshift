98d36bc8ff8e8e35be1e45cb3be176329d7a5d78
Cayo Hollanda <cayohollanda@hotmail.com>
2018-09-20 00:34:50 -0300

    atts

--- configure-oc.py
+++ configure-oc.py
@@ -8,3 +8,64 @@
 # Rodar comandos no bash
 import os
 
+# Acessar OS
+import platform
+
+# Método responsável por instalar uma dependência
+def instalaPacote(packageManager, pacote):
+    os.system(packageManager + pacote)
+
+# Método responsável por instalar o Docker
+def instalaDocker(pm):
+    if pm.find("yum"):
+        instalaPacote(pm, 'yum-utils \
+            device-mapper-persistent-data \
+            lvm2')
+        os.system("yum-config-manager \
+            --add-repo \
+            https://download.docker.com/linux/centos/docker-ce.repo")
+        instalaPacote(pm, 'docker-ce')
+        os.system("systemctl start docker")
+    else:
+        instalaPacote(pm, 'apt-transport-https \
+            ca-certificates \
+            curl \
+            software-properties-common')
+        os.system("curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -")
+        os.system("apt-key fingerprint 0EBFCD88")
+        os.system("sudo add-apt-repository \
+            'deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+            $(lsb_release -cs) \
+            stable'")
+        os.system("apt-get update")
+        instalaPacote(pm, 'docker-ce')
+
+# Método responsável por instalar o OpenShift Client Tools
+def instalaOcTools():
+    if not os.path.isfile("openshift-origin-client-tools-v3.10.0-dd10d17-linux-64bit.tar.gz"):
+        os.system("wget https://github.com/openshift/origin/releases/download/v3.10.0/openshift-origin-client-tools-v3.10.0-dd10d17-linux-64bit.tar.gz")
+
+    os.system("tar -xvf openshift-origin-client-tools-v3.10.0-dd10d17-linux-64bit.tar.gz")
+    os.system("cp openshift-origin-client-tools-v3.10.0-dd10d17-linux-64bit/oc /usr/local/bin/")
+
+# Verificando se o docker está instalado
+verifyDockerInstalled = os.system("docker version > /dev/null")
+
+# Verificando se o OpenShift Client Tools está instalado
+verifyOcInstalled = os.system("oc version > /dev/null")
+
+# Verificando sistema operacional
+verifyOperationSystem = os.path.isfile("cat /etc/redhat_release > /dev/null")
+
+packageManager = 'apt-get install -y '
+
+if verifyOperationSystem == True:
+    packageManager = 'yum install '
+
+if verifyDockerInstalled != 0:
+    instalaDocker(packageManager)
+
+if verifyOcInstalled != 0:
+    instalaOcTools()
+
+print("[*] Dependências instaladas com sucesso!")
--- up-cluster.py
+++ up-cluster.py
@@ -8,12 +8,21 @@
 # Rodar comandos no bash
 import os
 
+# Verificando se OpenShift Client Tools está instalado
+verifyIfOcIsInstalled = os.system("oc version > /dev/null")
+
+if verifyIfOcIsInstalled != 256:
+    print("Erro ao executar comando. Verifique se o OpenShift Client Tools está instalado.")
+    exit()
+
 # Coletando informações
 public_hostname = raw_input("[?] DNS ou IP interno da máquina virtual -> ")
+while public_hostname == '':
+    public_hostname = raw_input("[?] DNS ou IP interno da máquina virtual -> ")
+
 routing_suffix = raw_input("[?] IP público da máquina virtual -> ")
+while routing_suffix == '':
+    routing_suffix = raw_input("[?] IP público da máquina virtual -> ")
 
 # Rodando script
 commandRun = os.system("oc cluster up --skip-registry-check=true --public-hostname=%s --routing-suffix=%s" %(public_hostname, routing_suffix))
-
-if commandRun == 32512:
-    print("Erro ao executar comando. Verifique se o OpenShift Client Tools está instalado.")
